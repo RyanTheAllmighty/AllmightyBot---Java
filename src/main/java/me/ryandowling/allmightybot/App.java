9dc20457c85a92c2fcf9c2db703833864b2d5078
RyanTheAllmighty <rldowling03@gmail.com>
2014-12-06 20:41:01 +1100

    Get things working and in right folders

---
+++ src/main/java/me/ryandowling/allmightybot/App.java
@@ -0,0 +1,30 @@
+/*
+ * Allmighty Bot - https://github.com/RyanTheAllmighty/AllmightyBot
+ * Copyright (C) 2014 Ryan Dowling
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package me.ryandowling.allmightybot;
+
+public class App {
+
+    /**
+     * Store the Allmighty Bot instance so we can reference it easily for things.
+     */
+    public static AllmightyBot INSTANCE;
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
